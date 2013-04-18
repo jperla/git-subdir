7e83d5ee0fbf9201061b8297b299320d7f94985e
Joseph Perla <jperla@fb.com>
2013-04-18 10:29:52 -0700

    Add unit tests

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ import contextlib
 
 repo_name = 'pebbles'
 repo_url = 'https://github.com/jperla/pebbles'
-test_dirname = './test_dir'
+test_dirname = './sampledir'
 
 @contextlib.contextmanager
 def chdir(directory):
@@ -28,53 +28,104 @@ def make_test_dir_if_not_exists():
 
 def call_git_subdir(args):
     command = ['./git-subdir',] + args
-    print command
-    return subprocess.call(command)
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    return (p.wait(), stdout, stderr)
 
 def cleanup():
     if os.path.exists(test_dirname):
         shutil.rmtree(test_dirname)
-        shutil.rm('.subdirs')
+    if os.path.exists('.subdirs'):
+        os.remove('.subdirs')
 
-def vendortest(f):
+def setupcleanup(f):
     @contextlib.contextmanager
     def decorated(*args, **kwargs):
         cleanup()
         make_test_dir_if_not_exists()
-        f(*args, **kwargs)
-        cleanup()
+        try:
+            f(*args, **kwargs)
+        finally:
+            cleanup()
     decorated.__name__ = f.__name__
     return decorated
 
 def test_help():
-    assert call_git_subdir([]) == 0
-    #TODO: jperla: test output
+    r, o, e = call_git_subdir([])
+    assert r == 0
+    assert 'clone' in o
+    assert 'pull' in o
+    assert 'push' in o
+    assert 'rm' in o
 
-@vendortest
-def test_clone_master():
+def check_files_exist(new=False):
+    """Checks that the files from the old commit are around, and that new files are are not there."""
+    exists = lambda f: os.path.exists(os.path.join(test_dirname, repo_name, f))
+
+    for f in ['pebbles.js', 'README', 'test/index.html']:
+        assert exists(f)
+    for f in ['test/test2.js', 'test/index2.html']:
+        if new:
+            assert exists(f)
+        else:
+            assert not exists(f)
+
+@setupcleanup
+def test_clone_master_explicit():
     path = os.path.join(test_dirname, repo_name)
-    assert call_git_subdir(['clone', path, repo_url, 'master']) == 0
-    #TODO: jperla: test individual files
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'master'])
+    assert r == 0
+    check_files_exist(new=True)
 
     # calling twice fails
-    assert call_git_subdir(['clone', path, repo_url, 'master']) == 1
-    #TODO: jperla: test error message
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'master'])
+    assert r == 1
+    assert 'already cloned' in o
+    check_files_exist(new=True)
 
-@vendortest
+@setupcleanup
+def test_clone_master_implicit_and_rm():
+    path = os.path.join(test_dirname, repo_name)
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'master'])
+    assert r == 0
+    check_files_exist(new=True)
+
+    r, o, e = call_git_subdir(['rm', path])
+    assert not os.path.exists(path)
+
+@setupcleanup
 def test_clone_hash():
     path = os.path.join(test_dirname, repo_name)
-    assert call_git_subdir(['clone', path, repo_url, 'ae374469']) == 0
-    #TODO: jperla: test individual files
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'ae374469'])
+    assert r == 0
+    check_files_exist(new=False)
 
     # calling twice fails
-    assert call_git_subdir(['clone', path, repo_url, 'master']) == 1
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'master'])
+    assert r == 1
+    assert 'already cloned' in o
+    check_files_exist(new=False)
 
-@vendortest
-def test_pull():
+@setupcleanup
+def test_pull_explicit():
     path = os.path.join(test_dirname, repo_name)
 
-    #TODO: jperla: test individual files
-    assert call_git_subdir(['clone', path, repo_url, 'ae374469']) == 0
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'ae374469'])
+    assert r == 0
+    check_files_exist(new=False)
 
-    #TODO: jperla: test individual files
-    assert call_git_subdir(['pull', path, repo_url, 'master']) == 0
+    r, o, e = call_git_subdir(['pull', path, 'master'])
+    assert r == 0
+    check_files_exist(new=True)
+
+@setupcleanup
+def test_pull_implicit():
+    path = os.path.join(test_dirname, repo_name)
+
+    r, o, e = call_git_subdir(['clone', path, repo_url, 'ae374469'])
+    assert r == 0
+    check_files_exist(new=False)
+
+    r, o, e = call_git_subdir(['pull', path])
+    assert r == 0
+    check_files_exist(new=True)
